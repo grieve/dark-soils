5f3bdbd7ce5c5ec030e37a960c125ea2fcd82ef8
Ryan Grieve <ryan@rehabstudio.com>
2014-04-28 14:33:07 +0100

    narrative tweaks

--- src/narrative.js
+++ src/narrative.js
@@ -17,57 +17,57 @@ var Narrative = function(scene){
         wordWrap: true,
         wordWrapWidth: 250
     };
-
-    this.severity = 1;
 };
 
 Narrative.prototype = Object.create(Phaser.Group.prototype);
 Narrative.prototype.constructor = Narrative;
 
 Narrative.prototype.playChapter = function(name){
-    console.log(name);
     this.game.time.events.remove(this.timer);
     this.currentChapter = Script[name];
-    console.log(this.currentChapter);
     this.currentLine = 0;
     this.writeLine();
 };
 
 Narrative.prototype.writeLine = function(){
-    var script = this.currentChapter[this.currentLine];
+    var script = this.currentChapter[this.currentLine++];
+    var insev = script.severity;
+    var outsev = script.severity;
+    if (this.currentLine == this.currentChapter.length){
+        this.timer = this.game.time.events.add(2000 + script.duration, this.reset, this);
+        outsev = 30;
+    } else {
+        if (this.currentLine == 1){
+            insev = 10;
+        }
+        this.timer = this.game.time.events.add(2000 + script.duration, this.writeLine, this);
+    }
+
     if (this.line) this.line.destroy();
     this.line = new Phaser.Text(
         this.game,
         this.scene.player.x + 120,
         this.scene.player.y - 120,
-        script[2],
+        script.line,
         this.style
     );
 
-    console.log(script[2]);
-
     this.line.setShadow(2, 2, "rgba(0, 0, 0, 0.5)", 1);
     this.line.alpha = 0;
+    this.line.severity = insev;
     this.game.add.tween(this.line)
-        .to({alpha: 1}, 750, Phaser.Easing.Quadratic.out)
-        .to({alpha: 1}, script[1], Phaser.Easing.Quadratic.out)
-        .to({alpha: 0}, 750, Phaser.Easing.Quadratic.out)
+        .to({alpha: 1, severity: script.severity}, 750, Phaser.Easing.Quadratic.in)
+        .to({alpha: 1}, script.duration, Phaser.Easing.Quadratic.out)
+        .to({alpha: 0, severity: outsev}, 1000, Phaser.Easing.Quadratic.out)
         .start();
-    this.currentLine++;
-    if (this.currentLine == this.currentChapter.length){
-        this.timer = this.game.time.events.add(1500 + script[1] + script[3], this.reset, this);
-    } else {
-        this.timer = this.game.time.events.add(1500 + script[1] + script[3], this.writeLine, this);
-    }
 
-    this.severity = script[0];
     this.add(this.line);
 };
 
 Narrative.prototype.update = function(){
     if(this.line){
-        this.line.x = this.scene.player.x - 30 + Math.random() * this.severity;
-        this.line.y = this.scene.player.y - 120 + Math.random() * this.severity;
+        this.line.x = this.scene.player.x - 30 + Math.random() * this.line.severity;
+        this.line.y = this.scene.player.y - 120 + Math.random() * this.line.severity;
     }
 };
 
--- src/script.js
+++ src/script.js
@@ -1,48 +1,132 @@
 module.exports = {
     intro: [
-        [2, 3000, "Samuel didn't feel like himself anymore, he felt drained", 2500],
-        [2, 3000, "Like the very essence of life itself was slowly being leeched away", 2500],
-        [3, 3000, "He didn't like this feeling; he had to do something about it", 2500],
-        [1, 4500, "His thoughts turned to the ground beneath him, he felt... power", 1500],
-        [1, 10000, "<SPACE to dig>", 3000]
+        {
+        	severity: 1, duration: 3500,
+        	line: "Samuel didn't feel like himself anymore, he felt drained"
+        },
+        {
+        	severity: 1, duration: 3500,
+        	line: "Like the very essence of life itself was slowly being leeched away"
+        },
+        {
+        	severity: 1, duration: 3500,
+        	line: "He didn't like this feeling; he had to do something about it"
+        },
+        {
+        	severity: 1, duration: 4000,
+        	line: "His thoughts turned to the ground beneath him, he felt... power"
+        },
+        {
+        	severity: 1, duration: 7000,
+        	line: "<SPACE to dig>"
+        },
     ],
     firstpowerup: [
-        [2, 3000, "The relic Samuel found lying beneath him pulsated", 2500],
-        [1, 3000, "It's dark energies restored him, renewed him", 2500],
-        [3, 3000, "He knew he must find more", 1500]
+        {
+        	severity: 1, duration: 3000,
+        	line: "The relic Samuel found lying beneath him pulsated"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "It's dark energies restored him, renewed him"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "He knew he must find more"
+        },
     ],
     groundskeeper: [
-        [1, 3000, "Samuel heard the footsteps of someone approaching", 2500],
-        [2, 3000, "Our hero didn't know much, but he knew this wasn't a friend", 2500],
-        [3, 3000, "He could feel the waves of malice and contempt", 1500],
-        [4, 3000, "It would be best if he kept moving", 2500]
+        {
+        	severity: 1, duration: 3000,
+        	line: "Samuel heard the footsteps of someone approaching"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "Our hero didn't know much, but he knew this wasn't a friend"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "He could feel the waves of malice and contempt"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "It would be best if he kept moving"
+        },
     ],
     graves: [
-        [2, 3000, "Samuel had begun to notice the graves strewn about this place", 2500],
-        [2, 3000, "He wondered who would be buried in a place like this", 2500],
-        [2, 3000, "He decided he would dig into one and find out", 2500]
+        {
+        	severity: 1, duration: 3000,
+        	line: "Samuel had begun to notice the graves strewn about this place"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "He wondered who would be buried in a place like this"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "He decided he would dig into one and find out"
+        },
     ],
     lostsoul: [
-        [2, 3000, "A smokey form rose from the grave before Samuel", 1500],
-        [2, 3000, "He did not sense the same evil as the Groundskeeper", 1500],
-        [1, 3000, "Something told him this shade would not harm him", 1500]
+        {
+        	severity: 1, duration: 3000,
+        	line: "A smokey form rose from the grave before Samuel"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "He did not sense the same evil as the Groundskeeper"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "Something told him this shade would not harm him"
+        },
     ],
     zombie: [
-        [2, 2200, "A fetid stench erupted from the grave", 1000],
-        [3, 3000, "Along with a ravenous undead corpse", 2000],
-        [3, 3000, "Samuel wisely decides to run away", 2000]
+        {
+        	severity: 1, duration: 2200,
+        	line: "A fetid stench erupted from the grave"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "Along with a ravenous undead corpse"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "Samuel wisely decides to run away"
+        },
     ],
     nothing: [
-        [2, 3000, "This grave was completely empty", 2500],
-        [2, 3000, "Why would someone bury nothing?", 4500],
-        [3, 3000, "Unless the occupants had already left", 2500]
+        {
+        	severity: 1, duration: 3000,
+        	line: "This grave was completely empty"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "Why would someone bury nothing?"
+        },
+        {
+        	severity: 1, duration: 3000,
+        	line: "Unless the occupants had already left"
+        },
     ],
     beacons: [
-        [2, 3000, "Samuel found that if he concentrated, he could feel the power below him", 2500],
-        [1, 5000, "He stood still for a moment to focus", 4500]
+        {
+        	severity: 1, duration: 3000,
+        	line: "Samuel found that if he concentrated, he could feel the power below him"
+        },
+        {
+        	severity: 1, duration: 5000,
+        	line: "He stood still for a moment to focus"
+        },
     ],
     death: [
-        [3, 2000, "Samuel will not be remembered", 1000],
-        [3, 2000, "He will not be mourned", 1000]
+        {
+        	severity: 1, duration: 2000,
+        	line: "Samuel will not be remembered"
+        },
+        {
+        	severity: 1, duration: 2000,
+        	line: "He will not be mourned"
+        },
     ]
 };
